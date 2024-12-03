8be3353323b7e1a6eb1e3c966a60e740c1afe8a2
Giovana2024A <00001096955568sp@al.educacao.sp.gov.br>
2024-12-02 22:17:18 -0300

    projeto flash card
    .

---
+++ Flashcard.alura/perguntas.js
@@ -0,0 +1,32 @@
+criaCartao(
+    'Bíblica',
+    'Qual o livro mais antigo da Bíblia?',
+    'O livro de Jó'
+)
+
+criaCartao(
+    'Bíblica',
+    'O que significa Emanuel?',
+    'Deus Conosco'
+)
+
+criaCartao(
+    'Bíblica',
+    'Quanto tempo o povo de Israel ficou no Deserto?',
+    '40 anos'
+)
+
+criaCartao(
+    'Bíblica',
+    'De tudo que se deve guardar,guarda teu?',
+    'Coração'
+)
+
+criaCartao(
+    'Bíblica',
+    'Jesus tinha quantos discípulos?',
+    '12'
+)
+
+
+    
